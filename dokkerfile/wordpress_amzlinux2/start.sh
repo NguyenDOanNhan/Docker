f39eaf3b97ed071186c9947b0b7701478af7090c
Nguyễn Doãn Nhân <43739331+NguyenDOanNhan@users.noreply.github.com>
2021-03-09 14:17:20 +0700

    NhanND update

--- dokkerfile/wordpress_amzlinux2/start.sh
+++ dokkerfile/wordpress_amzlinux2/start.sh
@@ -5,12 +5,11 @@ sed -i -e "s/database_name_here/${DATABASE_NAME}/" /var/www/wordpress/wp-config.
 sed -i -e "s/username_here/${DATABASE_USER}/" /var/www/wordpress/wp-config.php
 sed -i -e "s/password_here/${DATABASE_PASSWORD_USER}/" /var/www/wordpress/wp-config.php
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
-# chay ngam tien trinh
 nohup /usr/libexec/mysqld --user=root &
 sleep 5
 mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('${PASSWORD_ROOTMYSQL}');FLUSH PRIVILEGES;"
 mysql -uroot -p${PASSWORD_ROOTMYSQL} -e "CREATE DATABASE ${DATABASE_NAME};"
 mysql -uroot -p${PASSWORD_ROOTMYSQL} -e "GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'localhost' IDENTIFIED BY '${DATABASE_PASSWORD_USER}';"
-mysql -uroot -p${PASSWORD_ROOTMYSQL} -e "GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASSWORD_USER}';"# create two user login
+mysql -uroot -p${PASSWORD_ROOTMYSQL} -e "GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASSWORD_USER}';"
 mysql -uroot -p${PASSWORD_ROOTMYSQL} -e "FLUSH PRIVILEGES;"
 nginx -g 'daemon off;'
