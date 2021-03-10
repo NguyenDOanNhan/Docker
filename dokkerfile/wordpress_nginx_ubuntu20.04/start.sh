2410ff23292679e8d07c4752c6db72461f8bd506
Nguyễn Doãn Nhân <43739331+NguyenDOanNhan@users.noreply.github.com>
2021-03-10 13:57:57 +0700

    NhanND update command

--- dokkerfile/wordpress_nginx_ubuntu20.04/start.sh
+++ dokkerfile/wordpress_nginx_ubuntu20.04/start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 /etc/init.d/php7.4-fpm start
-/etc/init.d/mysql start
+
 sed -i -e "s/database_name_here/${DATABASE_NAME}/" /var/www/wordpress/wp-config.php
 sed -i -e "s/username_here/${DATABASE_USER}/" /var/www/wordpress/wp-config.php
 sed -i -e "s/password_here/${DATABASE_PASSWORD_USER}/" /var/www/wordpress/wp-config.php
-
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+mysql_install_db --user=mysql --ldata=/var/lib/mysql
+/etc/init.d/mysql start --user=root
+mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('${PASSWORD_ROOTMYSQL}');FLUSH PRIVILEGES;"
 mysql -uroot -p${PASSWORD_ROOTMYSQL} -e "CREATE DATABASE ${DATABASE_NAME};"
 mysql -uroot -p${PASSWORD_ROOTMYSQL} -e "GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'localhost' IDENTIFIED BY '${DATABASE_PASSWORD_USER}';"
 mysql -uroot -p${PASSWORD_ROOTMYSQL} -e "GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASSWORD_USER}';"
 mysql -uroot -p${PASSWORD_ROOTMYSQL} -e "FLUSH PRIVILEGES;"
+
 nginx -g 'daemon off;'
